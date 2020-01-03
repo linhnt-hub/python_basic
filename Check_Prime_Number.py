6cd72a2a30e0c5016e547df5274ef7d59664411c
linhnt-hub <59143079+linhnt-hub@users.noreply.github.com>
2020-01-03 14:21:23 +0700

    Update Check_Prime_Number.py

--- Check_Prime_Number.py
+++ Check_Prime_Number.py
@@ -10,8 +10,8 @@
 #-------------------------------------------------------------------------------
 
 def main():
-    num=407
-    if num >1:
+    num=int(input("Nhap so nguyen can check: "))
+    if num>1:
         for i in range(2,num):
             if ( num%i ) ==0:
                 print(num,"is not a prime number")
