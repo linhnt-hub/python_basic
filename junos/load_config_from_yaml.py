af8d90a6331217d0e73380594c89c3cb58abb762
Nik <59143079+linhnt-hub@users.noreply.github.com>
2024-06-21 11:39:50 +0700

    Delete junos/load_config_from_yaml.py

--- junos/load_config_from_yaml.py
+++
@@ -1,5 +0,0 @@
-import yaml
-############## Import /opt/SVTECH-Junos-Automation/module_utils/PYEZ_BASE_FUNC.py ######################
-# Load config path from file
-config = yaml.load(open('app_config.yml',"r"), Loader=yaml.FullLoader)
-print(config.get('path_module_utils'))
