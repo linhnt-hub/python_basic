7c303b94afaf470dacf7c5261e861c7a0533a3ab
Nik <59143079+linhnt-hub@users.noreply.github.com>
2024-06-21 11:39:17 +0700

    Delete junos/check_duplicate_value_list.py

--- junos/check_duplicate_value_list.py
+++
@@ -1,3 +0,0 @@
-########## Check duplicate table/view #############
-dup_table= {x for x in list_table_result if list_table_result.count(x) > 1} # Check duplicate table
-dup_view= {x for x in list_view_result if list_view_result.count(x) > 1} # Check duplicate view
