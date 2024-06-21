07dd96d30774df077ec2905a7534996709d04cfc
Nik <59143079+linhnt-hub@users.noreply.github.com>
2024-06-21 11:02:09 +0700

    Delete get_variables_from_jinja.py

--- get_variables_from_jinja.py
+++
@@ -1,9 +0,0 @@
-def extract_variables(template_file):
-    with open(template_file, 'r') as f:
-        template_content = f.read()
-
-    # Use a regular expression to find Jinja variable syntax
-    variable_pattern = r'{{\s*(\w+)\s*}}'
-    variables = re.findall(variable_pattern, template_content)
-
-    return variables
